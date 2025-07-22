b2176b14f8fcb62506ebab942faad256ee6571c4
Mohammad Hasan <mohammad.hasan24069@gmail.com>
2025-07-22 14:33:37 +0500

    Refactor: use env vars for Firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,16 @@
-// firebaseConfig.ts
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4WGIWPFccAgZiqiDjDMPwbeQT8PFnIQo",
-  authDomain: "hanashi-beta.firebaseapp.com",
-  projectId: "hanashi-beta",
-  storageBucket: "hanashi-beta.firebasestorage.app",
-  messagingSenderId: "428591119484",
-  appId: "1:428591119484:web:b40803b47bf70ba1fc5df9",
-  measurementId: "G-XXND8L5ES7",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
